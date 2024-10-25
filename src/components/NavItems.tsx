c2c0aa774da38f81d99384e09732dc927dd14b1b
hishammfaume <hishammfaume@gmail.com>
2024-10-25 14:01:22 +0100

    Add NavItems component

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+"use client"
+
+const NavItems = () => {
+  return (
+    <div>NavItems</div>
+  )
+}
+
+export default NavItems
