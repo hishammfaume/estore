0646765a61f7e7073dc46aeee0a71aaff13fecc1
hishammfaume <hishammfaume@gmail.com>
2024-10-27 14:45:45 +0000

    Refactor package dependencies and update react-query version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from 'next/font/google';
-import { cn } from '../lib/utils';
+import { Inter } from "next/font/google";
+import { cn } from "../lib/utils";
 import Navbar from "../components/Navbar";
+import Providers from "@/components/Providers";
 
-
-
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +23,10 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
-          <Navbar/>
-          <div className="flex-grow flex-1">{children}</div>
-          
+          <Providers>
+            <Navbar />
+            <div className="flex-grow flex-1">{children}</div>
+          </Providers>
         </main>
       </body>
     </html>
