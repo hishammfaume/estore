f61bb470e87e80580ebc21db8ad96c00edda29f6
hishammfaume <hishammfaume@gmail.com>
2024-10-29 17:25:13 +0000

    Refactor VerifyEmail component and VerifyEmailPage
    - Extract VerifyEmail component from VerifyEmailPage
    - Move VerifyEmail component to src/components/VerifyEmail.tsx
    - Update imports in VerifyEmailPage to use the new location of VerifyEmail component

---
+++ src/components/VerifyEmail.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import { trpc } from '@/trpc/client'
+const VerifyEmail = () => {
+  return (
+    <div>VerifyEmail</div>
+  )
+}
+
+export default VerifyEmail
